49eb95ab4e66fe76e1ff4f9d35c13dadab91ec46
Kim-Lan <me@kim-lan.com>
2026-04-01 19:25:12 -0700

    Custom categories

---
+++ README.md
@@ -0,0 +1,5 @@
+# Kim-Lan's 25x25
+
+25x25 grouping game based on 2025 by Thomas Colthurst.
+
+Original from https://thomaswc.com/2025.html. UI Updates from [vessenes/2025is45x45](https://github.com/vessenes/2025is45x45). Forked from [Kim-Lan/pplushie-realm-45x45](https://github.com/Kim-Lan/pplushie-realm-45x45).
---
+++ cats.js
@@ -0,0 +1,55 @@
+const cats = {};
+
+// replace category names, keep quotes
+// paste lists between [] on the right side of the equal sign
+// example: cats["category name"] = ["item 1", "item 2", "item 3", ...];
+
+cats["minecraft effects"] = ["absorption", "bad omen", "conduit power", "darkness", "dolphin's grace", "fire resistance", "haste", "hero of the village", "infested", "invisibility", "jump boost", "levitation", "luck", "mining fatigue", "night vision", "poison", "regeneration", "resistance", "slow falling", "slowness", "speed", "strength", "water breathing", "weakness", "wither"];
+
+cats["minecraft structures"] = ["ancient city", "buried treasure", "mineshaft", "stronghold", "trail ruins", "trial chambers", "desert pyramid", "igloo", "jungle pyramid", "pillager outpost", "swamp hut", "village", "woodland mansion", "ruined portal", "ocean monument", "ocean ruins", "shipwreck", "bastion remnant", "nether fortress", "nether fossil", "end city", "amethyst geode", "desert well", "fossil", "dungeon"];
+
+cats["minecraft overworld biomes"] = ["ice spikes", "pale garden", "warm ocean", "desert", "jungle", "cold ocean", "sparse jungle", "dripstone caves", "savanna", "mushroom fields", "jagged peaks", "plains", "badlands", "meadow", "cherry grove", "beach", "lush caves", "deep dark", "windswept gravelly hills", "mangrove swamp", "forest", "flower forest", "taiga", "old growth pine taiga", "dark forest"];
+
+cats["minecraft passive mobs"] = ["allay", "armadillo", "axolotl", "bat", "camel", "cat", "chicken", "villager", "turtle", "cow", "donkey", "frog", "glow squid", "happy ghast", "horse", "mooshroom", "mule", "tropical fish", "parrot", "pig", "rabbit", "salmon", "sheep", "sniffer", "strider"];
+
+cats["twilight princess items"] = ["ball and chain", "bomb bag", "bomb", "bombling", "bottle", "clawshot", "coral earring", "dominion rod", "fishing rod", "gale boomerang", "hawkeye", "hero's bow", "horse call", "iron boots", "lantern", "ooccoo", "green chu jelly", "slingshot", "spinner", "ordon sword", "ordon shield", "hero's clothes", "magic armor", "great fairy's tears", "superb soup"];
+
+cats["zelda dungeons"] = ["inside the deku tree", "dodongo's cavern", "inside jabu-jabu's belly", "dragon roost cavern", "forbidden woods", "ice cavern", "tower of hera", "bottom of the well", "forest temple", "water temple", "turtle rock", "goron mines", "lakebed temple", "arbiter's grounds", "snowpeak ruins", "temple of time", "city in the sky", "palace of twilight", "skyview temple", "lanayru mining facility", "ancient cistern", "sandship", "fire sanctuary", "ganon's tower", "forsaken fortress"];
+
+cats["hamilton songs"] = ["alexander hamilton", "aaron burr, sir", "my shot", "the schuyler sisters", "you'll be back", "helpless", "satisfied", "wait for it", "ten duel commandments", "that would be enough", "dear theodosia", "the room where it happens", "washington on your side", "one last time", "burn", "blow us all away", "stay alive", "it's quiet uptown", "who lives, who dies, who tells your story", "cabinet battle #1", "say no to this", "schuyler defeated", "the adams administration", "hurricane", "the reynolds pamphlet"];
+
+cats["wicked things"] = ["elphaba", "glinda", "boq", "nessarose", "fiyero", "scarecrow", "tin man", "defying gravity", "shiz university", "the wizard", "dr. dillamond", "madame morrible", "chistery", "no one mourns the wicked", "popular", "dancing through life", "one short day", "what is this feeling?", "as long as you're mine", "no good deed", "for good", "dear old shiz", "the wizard and i", "i'm not that girl", "thank goodness"];
+
+cats["javascript array functions"] = ["sort", "concat", "copyWithin", "entries", "unshift", "fill", "filter", "find", "findIndex", "findLast", "flat", "flatMap", "forEach", "include", "indexOf", "join", "splice", "values", "pop", "push", "reduce", "reverse", "shift", "slice", "toString"];
+
+cats["data structures"] = ["array", "linked list", "struct", "tuple", "hash table", "hash map", "graph", "stack", "queue", "tree", "heap", "binary tree", "binary search tree", "red-black tree", "trie", "list", "set", "deque", "multimap", "multiset", "priority queue", "bit array", "doubly linked list", "min-max heap", "adjacency matrix"];
+
+cats["areas of math"] = ["abstract algebra", "number theory", "differential geometry", "harmonic analysis", "point-set topology", "combinatorics", "geometry", "algebra", "graph theory", "probability theory", "algebraic topology", "calculus", "category theory", "game theory", "complex analysis", "set theory", "algebraic geometry", "galois theory", "discrete", "field theory", "real analysis", "linear algebra", "trigonometry", "knot theory", "functional analysis"];
+
+cats["tv shows kim-lan has watched"] = ["how i met your mother", "psych", "the fosters", "umbrella academy", "friends", "heartstopper", "heartbreak high", "high school musical: the musical: the series", "ginny & georgia", "grey's anatomy", "orphan black", "orange is the new black", "the 100", "the good place", "squid game", "the flash", "legends of tomorrow", "black lightning", "cobra kai", "never have i ever", "supergirl", "avatar: the last airbender", "masterchef", "station 19", "scrubs"];
+
+cats["peak achievements"] = ["bundled up", "cryptogastronomy", "undead encounter", "competitive eating", "clutch", "knot tying", "resourcefulness", "speed climber", "disaster response", "leave no trace", "survivalist", "emergency preparedness", "lone wolf", "gourmand", "24 karat", "needlepoint", "megaentomology", "naturalist", "cool cucumber", "animal serenading", "bookworm", "happy camper", "esoterica", "bing bong", "mycology"];
+
+cats["countries visited in the amazing race"] = ["united states", "dominican republic", "mexico", "argentina", "brazil", "chile", "colombia", "france", "germany", "italy", "netherlands", "united kingdom", "switzerland", "morocco", "tanzania", "botswana", "china", "india", "japan", "thailand", "vietnam", "indonesia", "australia", "malaysia", "philippines"];
+
+cats["california cities"] = ["beverly hills", "anaheim", "berkeley", "palm springs", "bakersfield", "chula vista", "coachella", "compton", "los angeles", "san francisco", "san diego", "san jose", "palo alto", "eureka", "fresno", "sacramento", "riverside", "san gabriel", "long beach", "monterey", "oakland", "malibu", "oceanside", "solana beach", "big bear lake"];
+
+cats["wild cats"] = ["baby loaf", "andean mountain", "puma", "caracal", "serval", "kodkod", "margay", "geoffroy's", "canada lynx", "pampas", "colocolo", "eurasian lynx", "cheetah", "cougar", "leopard", "black-footed", "guina", "jaguar", "lion", "tiger", "snow leopard", "jaguarundi", "southern tigrina", "rusty-spotted", "african golden"];
+
+cats["season 37 amazing race challenges"] = ["who's ready for some ups and downs", "dance or sing", "who's ready to beat the competition?", "mochi or mawashi", "who's up for some kicks?", "fold it or fling it", "who's ready to rice to the occasion?", "penjor or pajegan", "coconuts or coral", "who wants to meet their match?", "who wants to drop into dubai?", "out to dry or for the birds", "who's got a steady hand?", "who's ready to ride a wave?", "picture perfect postcard puzzle", "identify 9 cheeses", "haystack or woodstack", "who's ready to fill 'er up?", "firewalking", "cyrillic cypher", "who wants to step back in time?", "mandolin or mozzarella", "grocery delivery", "who's feeling saucy?", "who's ready to go head-to-head?"];
+
+cats["switch games"] = ["mario kart 8 deluxe", "animal crossing: new horizons", "super smash bros. ultimate", "the legend of zelda: breath of the wild", "super mario odyssey", "pokémon scarlet and violet", "pokémon sword and shield", "the legend of zelda: tears of the kingdom", "super mario party", "new super mario bros. u deluxe", "super mario bros. wonder", "ring fit adventure", "pokémon brilliant diamond and shining pearl", "pokémon legends: arceus", "luigi's mansion 3", "mario party superstars", "splatoon 3", "super mario party jamboree", "super mario 3d all-stars", "super mario maker 2", "pokémon legends: z-a", "kirby and the forgotten land", "kirby star allies", "fire emblem: three houses", "the legend of zelda: echoes of wisdom"];
+
+cats["gamecube games"] = ["super smash bros. melee", "mario kart: double dash!!", "super mario sunshine", "the legend of zelda: the wind waker", "luigi's mansion", "metroid prime", "animal crossing", "mario party 7", "pokémon colosseum", "paper mario: the thousand-year door", "star fox adventures", "sonic adventure 2: battle", "pikmin", "super mario strikers", "f-zero gx", "the legend of zelda: twilight princess", "pokémon xd: gale of darkness", "sonic heroes", "kirby air ride", "mario power tennis", "pikmin 2", "mario party 4", "final fantasy crystal chronicles", "donkey konga", "mario superstar baseball"];
+
+cats["dairy products"] = ["bulgarian yogurt", "butter", "whipped cream", "buttermilk", "cheese", "condensed milk", "cottage cheese", "cream", "cream cheese", "crème fraîche", "curd", "custard", "dulce de leche", "evaporated milk", "eggnog", "frozen yogurt", "gelato", "yogurt", "goat milk", "greek yogurt", "ice cream", "whey", "sour cream", "yakult", "paneer"];
+
+cats["adobe creative cloud"] = ["acrobat", "premiere rush", "after effects", "animate", "audition", "bridge", "capture", "character animator", "dimension", "dreamweaver", "express", "fonts", "color", "illustrator", "incopy", "indesign", "lightroom", "lightroom classic", "media encoder", "photoshop", "prelude", "premiere pro", "stock", "xd", "portfolio"];
+
+cats["vegetables"] = ["arugula", "bok choy", "broccoli", "carrot", "daikon", "brussel sprout", "cabbage", "celery", "collard green", "corn", "potato", "kale", "lettuce", "spinach", "pea", "beetroot", "cauliflower", "yam", "asparagus", "jicama", "garlic", "onion", "shallot", "scallion", "leek"];
+
+cats["avatar: the last airbender characters"] = ["aang", "katara", "sokka", "appa", "momo", "zuko", "toph", "suki", "roku", "kyoshi", "ty lee", "zhao", "monk gyatso", "king bumi", "mai", "guru pathik", "princess yue", "chief hakoda", "iroh", "jet", "sparky sparky boom man", "zhao", "azula", "wan shi tong", "ozai"];
+
+cats["css properties"] = ["background", "color", "font-family", "border", "bottom", "box-shadow", "flex-direction", "cursor", "display", "line-height", "font-size", "font-weight", "height", "width", "margin", "padding", "z-index", "overflow", "position", "top", "visibility", "align-items", "justify-content", "animation", "box-sizing"];
+
+cats["indie games"] = ["outer wilds", "celeste", "hyper light drifter", "disco elysium", "peak", "hollow knight", "stardew valley", "terraria", "animal well", "inscryption", "the binding of isaac", "tunic", "to the moon", "crypt of the necrodancer", "gris", "the stanley parable", "cuphead", "firewatch", "kena: bridge of spirits", "slay the spire", "shovel knight", "starbound", "a short hike", "undertale", "omori"];
